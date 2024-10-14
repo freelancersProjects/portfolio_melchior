2466a8ebb7ad0f9e67eb75f34ff6d86aa9b0539d
Mathis Boulais <mathis.oxym@gmail.com>
2024-10-14 08:53:31 +0200

    branch

--- templates/home.php
+++ templates/home.php
@@ -1,5 +1,24 @@
-<section class="hero">
-    <div class="hero-content">
-        <h1>Melchior Reynaud</h1>
-    </div>
-</section>
+<div class="container container-global">
+    <section>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="img/accueil.jpg" alt="accueil" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </section>
+
+    <section>
+    </section>
+
+    <section>
+    </section>
+
+    <section>
+    </section>
+
+    <section>
+    </section>
+</div>
