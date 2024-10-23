903c9d19d6cd63c923c0cee2ef056f7b8f076f0c
Mathis Boulais <mathis.boulais@naxos.fr>
2024-10-23 13:42:53 +0200

    global et biography

--- templates/home.php
+++ templates/home.php
@@ -1,14 +1,20 @@
 <div class="container container-global">
-    <section>
-        <div class="row">
-            <div class="col-md-6">
-                <img src="img/accueil.jpg" alt="accueil" class="img-fluid">
-            </div>
-            <div class="col-md-6">
+<section class="biography-section">
+        <div class="container">
+            <h1 class="text-center mb-4">Biographie</h1> <!-- Titre centré -->
 
+            <div class="row align-items-center">
+                <div class="col-md-6"> <!-- Colonne pour le texte -->
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec viverra est. Mauris vitae velit quis sapien dignissim tincidunt. Nulla facilisi. Phasellus et fermentum justo. Aliquam erat volutpat. Quisque euismod efficitur libero, vel vehicula metus. Sed vel suscipit nulla. 
+                    </p>
+                </div>
+                <div class="col-md-6"> <!-- Colonne pour l'image -->
+                    <img src="/portfolio_melchior/public/img/melchior_1024.jpg" class="img-fluid" alt="Photo de Melchior">
+                </div>
             </div>
         </div>
-    </section>
+</section>
 
     <section>
     </section>
