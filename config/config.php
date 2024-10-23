0480d315d6d276e0df06a63f8f2b3d16715faf6d
Titi <tristanfio05@gmail.com>
2024-10-23 21:02:06 +0200

    Uncomment config

--- config/config.php
+++ config/config.php
@@ -2,29 +2,29 @@
 
 function loadEnv($file)
 {
-//     if (!file_exists($file)) {
-//         return;
-//     }
-//     $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-//     foreach ($lines as $line) {
-//         if (strpos(trim($line), '#') === 0) {
-//             continue;
-//         }
-//         list($key, $value) = explode('=', $line, 2);
-//         putenv(trim($key) . '=' . trim($value));
-//     }
-// }
+    if (!file_exists($file)) {
+        return;
+    }
+    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+        list($key, $value) = explode('=', $line, 2);
+        putenv(trim($key) . '=' . trim($value));
+    }
+}
 
-// loadEnv(__DIR__ . '/../.env');
+loadEnv(__DIR__ . '/../.env');
 
-// define('DB_HOST', getenv('DB_HOST'));
-// define('DB_NAME', getenv('DB_NAME'));
-// define('DB_USER', getenv('DB_USER'));
-// define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
-// try {
-//     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASSWORD);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     die("Erreur de connexion à la base de données : " . $e->getMessage());
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
