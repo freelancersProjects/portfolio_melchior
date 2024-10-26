60c22dce4cd965acf83796e97cf7eca135304d11
Mathis Boulais <mathis.oxym@gmail.com>
2024-10-26 15:07:06 +0200

    Home + opti

--- templates/components/header.php
+++ templates/components/header.php
@@ -4,38 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
     <link rel="stylesheet" href="../public/css/styles.css">
     <!-- JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="../public/js/script.js"></script>
     <!-- FONT -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Della+Respira&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Melchior Portfolio</title>
 </head>
 
-<header class="background-header font-family-della-respira font-weight-regular">
+<header class="background-header font-family-della-respira font-weight-regular container-fluid">
     <nav class="p-3 d-flex justify-content-between align-items-center">
-        <a href="/portfolio_melchior/public/" class="text-white mx-2 fs-5 font-weight-regular">Melchior</a>
+        <a href="/portfolio_melchior/public/" class="text-white mx-5 fs-5 font-weight-regular titre-header">Melchior</a>
         <ul class="d-flex">
             <li>
-                <a href="#biography" class="text-white mx-2">Biographie</a>
+                <a href="#biography" class="text-white mx-5">Biographie</a>
             </li>
             <li>
-                <a href="#mes-oeuvres" class="text-white mx-2">Mes oeuvres</a>
+                <a href="#mes-oeuvres" class="text-white mx-5">Mes oeuvres</a>
             </li>
             <li>
-                <a href="#contact" class="text-white mx-2">Me contacter</a>
+                <a href="#contact" class="text-white mx-5">Me contacter</a>
             </li>
         </ul>
     </nav>
     <div class="d-flex align-items-center justify-content-center h-100">
-        <div class="text-center text-white">
+        <div class="text-center text-white textHeader">
             <h1>Melchior Reynaud</h1>
         </div>
-    </nav>
+    </div>
 </header>
--- templates/home.php
+++ templates/home.php
@@ -2,7 +2,7 @@
     <div class="bg-gray p-5">
         <div class="container">
             <div class="row">
-                <h2 class="text-center font-family-della-respira mb-5">Biographie</h2>
+                <h2 class="text-center font-family-della-respira titre-glob mb-5">Biographie</h2>
                 <div class="col-md-7">
                     <p class="font-family-montserrat font-weight-semi-bold color-dark-blue fs-15px">
                         Pianiste depuis son jeune âge, Melchior REYNAUD suit une formation classique et intègre au lycée la classe <strong>TMD</strong> (Technique de la Musique et de la Danse) à Annecy.
@@ -33,9 +33,56 @@
             </div>
         </div>
     </div>
-    <div class="text-center">
-        <iframe src="https://player.vimeo.com/video/952561476" width="800" height="600" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    <div class="text-center mt-5">
+        <iframe src="https://player.vimeo.com/video/952561476" width="1280" height="720" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    </div>
+
+</section>
+
+<section id="mes-oeuvres">
+<div class="bg-gray p-5 mt-5">
+        <?php
+        include 'components/artworks.php';
+        ?>
     </div>
 </section>
 
-<section id="artworks"></section>
+<section id="contact" class="contact-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="contact-title titre-glob">Formulaire de contact</h2>
+
+                <form class="contact-form">
+                    <div class="form-group">
+                        <label for="name">Votre nom</label>
+                        <input type="text" id="name" class="form-control" placeholder="Votre nom">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Adresse mail</label>
+                        <input type="email" id="email" class="form-control" placeholder="Adresse mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="subject">Sujet</label>
+                        <input type="text" id="subject" class="form-control" placeholder="Sujet">
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Votre message</label>
+                        <textarea id="message" class="form-control" rows="4" placeholder="Votre message"></textarea>
+                    </div>
+                    <button type="submit" class="contact-submit-btn">Envoyer</button>
+                </form>
+            </div>
+
+            <div class="col-md-6 container-social">
+                <h3 class="contact-social-title">Réseaux sociaux</h3>
+                <ul class="contact-social-list">
+                    <li><a href="#" class="contact-social-link"><i class="fas fa-envelope"></i></a></li>
+                    <li><a href="#" class="contact-social-link"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#" class="contact-social-link"><i class="fab fa-soundcloud"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
