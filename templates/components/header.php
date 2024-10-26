059855b5be0f2864a611a1c29208f17df5177e41
Mathis Boulais <mathis.oxym@gmail.com>
2024-10-26 17:24:00 +0200

    change script

--- templates/components/header.php
+++ templates/components/header.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="../public/css/styles.css">
     <!-- JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="../public/js/script.js"></script>
     <!-- FONT -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
@@ -39,14 +38,5 @@
         </div>
     </div>
 </header>
+<script src="../public/js/script.js"></script>
 
-<script>
-    if (window.location.pathname !== '/portfolio_melchior/public/') {
-        document.querySelector('header').classList.remove('background-header');
-        document.querySelectorAll('.text-white').forEach(element => {
-            element.classList.remove('text-white');
-            element.classList.add('text-black');
-        });
-        document.getElementById('main-name').style.display = 'none';
-    }
-</script>
